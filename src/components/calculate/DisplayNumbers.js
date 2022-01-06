a2598f0d52fc50b76b3911fbbee2c4a8933d63a4
DanielNicolae <deadlypoet@protonmail.com>
2022-01-06 23:26:37 +0200

    I fixed the background.

--- src/components/calculate/DisplayNumbers.js
+++ src/components/calculate/DisplayNumbers.js
@@ -4,18 +4,18 @@ import Number from './Number';
 import './DisplayMessage.css';
 import Images from '../images/Images';
 
-const DisplayNumbers = ({numbers, sides}) => {
+const DisplayNumbers = ({ numbers, sides }) => {
     if (numbers.length > 0) {
         return (
-            <div  className="message container">
+            <div className="message container">
                 <DisplayMessage sides={sides} />
-                
-                <Images  sides={sides} />
-                
+
+                <Images sides={sides} />
+
                 <ul>
                     {numbers.map((nr) => (
-                            <Number key={nr.id} number={nr.n} sides={sides} />
-                        )
+                        <Number key={nr.id} number={nr.n} sides={sides} />
+                    )
                     )}
                 </ul>
             </div>
